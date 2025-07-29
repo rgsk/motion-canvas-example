87cf3fa3342a073299878b5ddc2653989a0ba15c
rgsk <guptarahul70322@gmail.com>
2025-07-30 00:22:39 +0530

    fix

--- src/project.ts
+++ src/project.ts
@@ -1,6 +1,9 @@
+import { parser } from "@lezer/javascript";
+import { Code, LezerHighlighter } from "@motion-canvas/2d";
 import { makeProject } from "@motion-canvas/core";
-import customComponentSwitch from "./examples/customComponentSwitch?scene";
+import main from "./scenes/main?scene";
 
+Code.defaultHighlighter = new LezerHighlighter(parser);
 export default makeProject({
-  scenes: [customComponentSwitch],
+  scenes: [main],
 });
--- src/scenes/main.tsx
+++ src/scenes/main.tsx
@@ -1,3 +1,23 @@
-import { makeScene2D } from "@motion-canvas/2d";
+import { Code, makeScene2D } from "@motion-canvas/2d";
+import { all, createSignal, waitFor } from "@motion-canvas/core";
 
-export default makeScene2D(function* (view) {});
+export default makeScene2D(function* (view) {
+  const message = createSignal(`Hello, world!`);
+  const body = createSignal(() => `console.log('${message()}');`);
+
+  view.add(
+    <Code
+      fontSize={28}
+      offsetX={-1}
+      x={-400}
+      code={() => `\
+function hello() {
+  ${body()}
+}`}
+    />
+  );
+
+  yield* waitFor(0.3);
+  yield* all(message("Goodbye, world!", 0.6));
+  yield* waitFor(0.3);
+});
