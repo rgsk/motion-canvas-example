42fe8fc69a034d25ed20fe821d2f4543e1a0ff45
rgsk <guptarahul70322@gmail.com>
2025-07-31 21:44:13 +0530

    fix

--- src/project.ts
+++ src/project.ts
@@ -1,9 +1,9 @@
 import { parser } from "@lezer/javascript";
 import { Code, LezerHighlighter } from "@motion-canvas/2d";
 import { makeProject } from "@motion-canvas/core";
-import insertionSort from "./dsa/insertionSort?scene";
+import countSort from "./external/countSort?scene";
 
 Code.defaultHighlighter = new LezerHighlighter(parser);
 export default makeProject({
-  scenes: [insertionSort],
+  scenes: [countSort],
 });
